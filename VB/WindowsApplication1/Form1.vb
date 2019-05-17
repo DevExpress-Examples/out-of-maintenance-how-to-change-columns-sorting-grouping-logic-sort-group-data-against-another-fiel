cfee35ca5165ea0b2ebdc44690b9174167cc25b3
DevExpressExampleBot <fakeEmail>
2019-05-17 23:31:37 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/Form1.vb
@@ -1,35 +1,35 @@
-﻿Imports System
+Imports System
 Imports System.Data
 Imports System.Windows.Forms
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
+    Partial Public Class Form1
+        Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			invoicesBindingSource.DataSource = GetInvoicesDataTable()
-			colUnitPrice.FieldNameSortGroup = "ExtendedPrice"
-			colUnitPrice.SortOrder = DevExpress.Data.ColumnSortOrder.Ascending
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            invoicesBindingSource.DataSource = GetInvoicesDataTable()
+            colUnitPrice.FieldNameSortGroup = "ExtendedPrice"
+            colUnitPrice.SortOrder = DevExpress.Data.ColumnSortOrder.Ascending
+        End Sub
 
-		Private Function GetInvoicesDataTable() As DataTable
-			Dim table As New DataTable()
-			table.TableName = "Invoices"
-			table.Columns.Add(New DataColumn("ProductName", GetType(String)))
-			table.Columns.Add(New DataColumn("Discount", GetType(Integer)))
-			table.Columns.Add(New DataColumn("Quantity", GetType(Integer)))
-			table.Columns.Add(New DataColumn("UnitPrice", GetType(Double)))
-			table.Columns.Add(New DataColumn("ExtendedPrice", GetType(Double)))
-			Dim random As New Random()
-			For i As Integer = 0 To 19
-				Dim index As Integer = i + 1
-				table.Rows.Add("Product " & index, index * 2, index Mod 9, (index + random.Next(1, 10)) * 1000R, (index + random.Next(1, 10)) * 1000R + index * 100R)
-			Next i
-			Return table
-		End Function
-	End Class
+        Private Function GetInvoicesDataTable() As DataTable
+            Dim table As DataTable = New DataTable()
+            table.TableName = "Invoices"
+            table.Columns.Add(New DataColumn("ProductName", GetType(String)))
+            table.Columns.Add(New DataColumn("Discount", GetType(Integer)))
+            table.Columns.Add(New DataColumn("Quantity", GetType(Integer)))
+            table.Columns.Add(New DataColumn("UnitPrice", GetType(Double)))
+            table.Columns.Add(New DataColumn("ExtendedPrice", GetType(Double)))
+            Dim random As Random = New Random()
+            For i As Integer = 0 To 20 - 1
+                Dim index As Integer = i + 1
+                table.Rows.Add("Product " & index, index * 2, index Mod 9, (index + random.[Next](1, 10)) * 1000.0R, (index + random.[Next](1, 10)) * 1000.0R + index * 100.0R)
+            Next
+            Return table
+        End Function
+    End Class
 End Namespace
